b4e03abc0ab20dcf9ec0e84e7d802da55f908e7b
PabloPetit <pablopetit@gmail.com>
2016-10-18 21:00:38 +0200

    Sun light

--- Code/Etherion/Assets/Scripts/Environement/RotateAroundMapCenter.cs
+++ Code/Etherion/Assets/Scripts/Environement/RotateAroundMapCenter.cs
@@ -4,10 +4,11 @@ using System.Collections;
 public class RotateAroundMapCenter : MonoBehaviour {
 
 	public float speed = 0.8f;
+	public Vector3 axis = Vector3.forward;
 	// Use this for initialization
 	
 	// Update is called once per frame
 	void Update () {
-		gameObject.transform.RotateAround (Vector3.zero, Vector3.forward, speed * Time.deltaTime);
+		gameObject.transform.RotateAround (Vector3.zero, axis, speed * Time.deltaTime);
 	}
 }
---
+++ Code/Etherion/Assets/Scripts/Environement/RotateAroundObject.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateAroundObject : MonoBehaviour {
+
+	public float speed = 0.8f;
+	public Vector3 axis = Vector3.forward;
+	public GameObject attracter;
+
+
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.RotateAround (attracter.transform.position, axis, speed * Time.deltaTime);
+	}
+}
