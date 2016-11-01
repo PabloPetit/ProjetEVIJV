abf9b7a64f2ca6413137b81ae5f5d0f04c84b296
PabloPetit <pablopetit@gmail.com>
2016-11-01 18:47:48 +0100

    sync

--- Code/Etherion/Assets/Scripts/Projectile/Flare.cs
+++ Code/Etherion/Assets/Scripts/Projectile/Flare.cs
@@ -1,15 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Flare : MonoBehaviour {
+public class Flare : Projectile {
 
-	// Use this for initialization
-	void Start () {
-	
+	float gravityDeviation = 2f;
+
+	protected float acceleration;
+	protected float ascendingTime;
+	protected float descendingSpeed;
+	protected float intensity;
+	protected float lightRange;
+
+	protected Light light;
+
+
+	public static GameObject Create(GameObject owner, GameObject prefab, Transform barrel, float speed, float range, float acceleration, float ascendingTime, float descendingSpeed,float intensity, float lightRange){
+		GameObject projectile = Projectile.Create (owner, prefab, barrel,speed,range);
+		Flare flare = projectile.GetComponent<Flare> ();
+		flare.acceleration = acceleration;
+		flare.ascendingTime = ascendingTime;
+		flare.descendingSpeed = descendingSpeed;
+		flare.intensity = intensity;
+		flare.lightRange = lightRange;
+		flare.light = projectile.GetComponent<Light> ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+
+	void FixedUpdate () {
+		if (timer < ascendingTime) {
+			speed += acceleration * Time.fixedDeltaTime;
+		} else {
+
+
+		}
+
+		base.FixedUpdate ();
 	}
 }
