c728fab2b4e5de62ab5391d63dbaa72155e20846
Therisse <laetitia.zaleski@gmail.com>
2016-11-06 17:33:34 +0100

    wander ok chase pas ok

--- Code/Etherion/Assets/Scripts/Creature/Monstres/PatrollAndChase.cs
+++ Code/Etherion/Assets/Scripts/Creature/Monstres/PatrollAndChase.cs
@@ -4,19 +4,19 @@ using System.Collections;
 public class PatrollAndChase : MonoBehaviour 
 {
 	public float enemySpeed = 5.0f;
-	public Transform myTarget;
-	public GameObject go;
+	private Transform myTarget;
+	private GameObject go;
 	private Vector3 startPosition;
 	private bool isWandering = true;
-	public bool inCombat = false;
+	private bool inCombat = false;
 	public float enemyWanderSpeed = 30.0f;
-	public float wanderRange = 200.0f;
-	public float wanderDelayTimer;
-	public float distanceToCombat = 50.0f;
+	public float wanderRange = 100.0f;
+	private float wanderDelayTimer;
+	public float distanceToCombat = 100.0f;
 	public float distanceToDropCombat = 200.0f;
-	public bool chasingPlayer = false;
-	public NavMeshAgent agent;
-	public bool isReturning = false;
+	private bool chasingPlayer = false;
+	private NavMeshAgent agent;
+	private bool isReturning = false;
 
 
 	void Awake()
@@ -26,14 +26,15 @@ public class PatrollAndChase : MonoBehaviour
 		agent.speed  = enemyWanderSpeed;
 		startPosition = this.transform.position;
 	}
+
 	void Update () 
 	{
-		Debug.Log(transform.position);
+		//Debug.Log(transform.position);
 
 		startWander();
-		float distanceFromPlayer = Vector3.Distance(transform.position, go.transform.position);
+		/*float distanceFromPlayer = Vector3.Distance(transform.position, go.transform.position);
 		float distanceChasedCombat = Vector3.Distance(transform.position, startPosition);
-
+		//Debug.Log(distanceFromPlayer);
 		if(distanceFromPlayer <= distanceToCombat && !isReturning)
 		{
 			chasingPlayer = true;
@@ -49,28 +50,45 @@ public class PatrollAndChase : MonoBehaviour
 			myTarget = go.transform;
 			agent.SetDestination(myTarget.transform.position);
 			agent.speed = enemySpeed;
-		}
+		}*/
 
 	}
+
 	void startWander()
 	{
 
 		wanderDelayTimer -=Time.deltaTime;
+		Debug.Log(wanderDelayTimer);
 		if(wanderDelayTimer <= 0 && !chasingPlayer)
 		{
 			Wander();
-			wanderDelayTimer = Random.Range(5, 10.0f);
+			wanderDelayTimer = Random.Range(2.0f, 4.0f);
+			//Debug.Log(wanderDelayTimer);
 		}
 	}
+
 	void Wander()
 	{
-		agent.speed = enemyWanderSpeed;
+		/*agent.speed = enemyWanderSpeed;
 		Vector3 navDestination = Random.insideUnitSphere * wanderRange;
 		navDestination += transform.position;
 		NavMeshHit hit;
 		NavMesh.SamplePosition (navDestination,out hit, wanderRange,1);
-		navDestination = hit.position;
-		agent.SetDestination (navDestination);
+		navDestination = hit.position;*/
+
+		agent.speed = enemyWanderSpeed;
+		Vector3 destination = startPosition + new Vector3 (Random.Range (-wanderRange, wanderRange),0 /*transform.position.y*/, Random.Range(-wanderRange, wanderRange));
+		newDestination(destination);
+
+		//agent.SetDestination (navDestination);
 	}
+
+	public void newDestination(Vector3 targetPoint)
+	{
+		agent.SetDestination (targetPoint);
+		Debug.Log("new destination");
+		Debug.Log(targetPoint);
+	}
+
 
 }
