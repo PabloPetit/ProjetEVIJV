7a0b832424782fe47e4f7a6e872ed7689b7eaa29
PabloPetit <pablopetit@gmail.com>
2016-11-13 05:18:13 +0100

    Lights

---
+++ Code/Etherion/Assets/Scripts/UI/HUD/Fps.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Fps : MonoBehaviour
+{
+
+	Text text;
+
+	public int refreshTime;
+
+	int counter;
+
+	void Start ()
+	{
+		text = GetComponent<Text> ();
+		counter = 0;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		counter++;
+		if (counter >= refreshTime) {
+			text.text = "FPS : " + (int)(1f / Time.deltaTime);
+			counter = 0;
+		}
+
+	}
+}
--- Code/Etherion/Assets/Scripts/UI/HUD/LifeBar.cs
+++ Code/Etherion/Assets/Scripts/UI/HUD/LifeBar.cs
@@ -11,18 +11,32 @@ public class LifeBar : MonoBehaviour
 
 	private EventName lifeBarEvent;
 
+	private Image image;
+
+	private GameObject fillArea;
+
 	void Start ()
 	{
 		slider = GetComponent<Slider> ();
-
+		image = GetComponentInChildren<Image> ();
+		fillArea = transform.Find ("Fill Area").gameObject;
 	}
 
 
 	void Set (object[] param = null)
 	{
 		float percent = (float)param [0];
-
 		slider.value = slider.maxValue * percent;
+		if (percent < .25f) {
+			image.color = Color.red;
+		} else {
+			image.color = Color.green;
+		}
+		if (percent <= 0f) {
+			fillArea.SetActive (false);
+		} else {
+			fillArea.SetActive (true);
+		}
 	}
 
 	void OnEnable ()
