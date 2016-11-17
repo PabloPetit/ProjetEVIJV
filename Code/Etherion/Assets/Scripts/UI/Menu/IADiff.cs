2e1bdadf6ec2ed2289881752e65beb6d2387bf6c
Therisse <laetitia.zaleski@gmail.com>
2016-11-17 20:38:06 +0100

    commit

---
+++ Code/Etherion/Assets/Scripts/UI/Menu/IADiff.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class IADiff : MonoBehaviour {
+	public int s = 1;
+
+	void Update ()
+	{
+		if (s == 0) { // easy
+		
+		}
+		if (s == 1) { // medium
+
+		}
+		if (s == 2) { // hard
+
+		}
+	}
+
+
+}
