4907327b4910d593f89bfe7c7a103732d7320bfd
Therisse <laetitia.zaleski@gmail.com>
2016-11-17 16:42:03 +0100

    menu

--- Code/Etherion/Assets/Scripts/UI/Menu/Rotate.cs
+++ Code/Etherion/Assets/Scripts/UI/Menu/Rotate.cs
@@ -11,10 +11,10 @@ public class Rotate : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		// Rotate the object around its local X axis at 1 degree per second
-		transform.Rotate(Vector3.right * Time.deltaTime * 10);
+		transform.Rotate(Vector3.right * Time.deltaTime * 1);
 
 		// ...also rotate around the World's Y axis
-		transform.Rotate(Vector3.up * Time.deltaTime *10, Space.World);
+		//transform.Rotate(Vector3.up * Time.deltaTime *3, Space.World);
 	
 	}
 }
