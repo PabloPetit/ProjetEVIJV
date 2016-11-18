b30f26832911f9da3e2c0518ad10856f8aa4b49f
Therisse <laetitia.zaleski@gmail.com>
2016-11-18 17:10:54 +0100

    Timer

--- Code/Etherion/Assets/Scripts/UI/HUD/Score.cs
+++ Code/Etherion/Assets/Scripts/UI/HUD/Score.cs
@@ -1,11 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Score : MonoBehaviour {
 
+	public static string Score_CHANNEL = "score";
+
+	private EventName scoreEvent;
+
+	Text text;
+
+	int MaxScore = GameManager.targetScore;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+		text = GetComponentInChildren<Text> ();
+		text.text = "0-0 ("+;
 	}
 	
 	// Update is called once per frame
--- Code/Etherion/Assets/Scripts/UI/HUD/TimeChannel.cs
+++ Code/Etherion/Assets/Scripts/UI/HUD/TimeChannel.cs
@@ -1,18 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class TimeChannel : MonoBehaviour
 {
+	public static string TIME_CHANNEL = "timeChannel";
+
+	private EventName timeChannelEvent;
+
+	Text text;
+	public float timer = 0.0f;
+	public float maxTime;
+
+	public int remainingMin;
+	public int remainingSec;
+
+
 
 	// Use this for initialization
 	void Start ()
 	{
-	
+		text = GetComponentInChildren<Text> ();
+		maxTime = GameManager.maxTime * 60;
+
+		remainingMin =  GameManager.maxTime;
+		remainingSec = 0;
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
+		timer += Time.deltaTime;
+
+		text.text = (int)((maxTime - timer) / 60) + " : " + (int)((maxTime - timer) % 60);
 	
 	}
 }
