0378b1ba178c74552fc1be16ef531d28d3984f60
Therisse <laetitia.zaleski@gmail.com>
2016-11-18 15:07:17 +0100

    killlog

--- Code/Etherion/Assets/Scripts/UI/HUD/KillLog.cs
+++ Code/Etherion/Assets/Scripts/UI/HUD/KillLog.cs
@@ -1,10 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+
 
 public class KillLog : MonoBehaviour {
 
+
+	public static string KILL_LOG_CHANNEL = "killLog";
+
+	private EventName killLogEvent;
+
+	Text text;
+
+	private float timeTillFade = 4.0f ;
+
+	private int SizeQueue = 6;
+
+	private LimitedQueue<string> Queue = new LimitedQueue<string>(SizeQueue);
+
+
 	// Use this for initialization
 	void Start () {
+		text = GetComponentInChildren<Text> ();
 	
 	}
 	
