584aa573d102519c0dc65319aae9e08aad7af534
PabloPetit <pablopetit94@gmail.com>
2016-12-06 18:34:54 +0100

    Sky Manager

---
+++ Code/Etherion/Assets/Scripts/Environement/SkyManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkyManager : MonoBehaviour {
+
+	public static float DAY_LENGTH = 24f;
+	public static float MORNING = 8f;
+	public static float NOON = 12f;
+	public static float NIGHT = 20f;
+
+	public float speed = 0.8f;
+	public Vector3 axis = Vector3.forward;
+
+	public float time;
+	public int hours;
+	public int minutes;
+	public int seconds;
+
+	public ArrayList nightCallBack;
+	public ArrayList dayCallBack;
+
+
+	void Start(){
+		
+	}
+
+	void FixedUpdate(){
+		gameObject.transform.RotateAround (Vector3.zero, axis, speed * Time.fixedDeltaTime);
+		SetHour ();
+		Debug.Log (time+" - Heure  "+hours+" : "+minutes+" : "+seconds);
+	}
+
+    void SetHour(){
+		time = (12f + (transform.rotation.eulerAngles.z) / (360f / 24f)) % 24f;
+		hours = (int)time;
+		minutes = (int)((time % 1f) * 60f);
+		seconds =  (int)((time % 0.01f) * 60f);
+	}
+		
+}
---
+++ Code/Etherion/Assets/Scripts/Sound/SoundSystem.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundSystem : MonoBehaviour {
+
+
+	public AudioClip backround;
+	public AudioClip day;
+	public AudioClip night;
+	public AudioClip battle;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
