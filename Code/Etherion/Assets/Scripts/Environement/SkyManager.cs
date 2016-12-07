c8c7f7511f4f550309dec9ead5ee413ec254cef8
PabloPetit <pablopetit@gmail.com>
2016-12-07 10:16:23 +0100

    Sound System

--- Code/Etherion/Assets/Scripts/Environement/SkyManager.cs
+++ Code/Etherion/Assets/Scripts/Environement/SkyManager.cs
@@ -1,12 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class SkyManager : MonoBehaviour {
+public class SkyManager : MonoBehaviour
+{
 
 	public static float DAY_LENGTH = 24f;
-	public static float MORNING = 8f;
+	public static float MORNING = 7f;
 	public static float NOON = 12f;
-	public static float NIGHT = 20f;
+	public static float NIGHT = 17f;
 
 	public float speed = 0.8f;
 	public Vector3 axis = Vector3.forward;
@@ -16,25 +18,62 @@ public class SkyManager : MonoBehaviour {
 	public int minutes;
 	public int seconds;
 
-	public ArrayList nightCallBack;
-	public ArrayList dayCallBack;
+	public List<EventName> nightCallBack;
+	public List<EventName> dayCallBack;
 
+	public bool day;
 
-	void Start(){
-		
+	public void Awake ()
+	{
+		nightCallBack = new List<EventName> ();
+		dayCallBack = new List<EventName> ();
+		day = (time > MORNING && time < NIGHT);
 	}
 
-	void FixedUpdate(){
+	void FixedUpdate ()
+	{
 		gameObject.transform.RotateAround (Vector3.zero, axis, speed * Time.fixedDeltaTime);
 		SetHour ();
-		Debug.Log (time+" - Heure  "+hours+" : "+minutes+" : "+seconds);
+
+		if (day && !(time > MORNING && time < NIGHT)) {
+			day = false;
+			ProcessNightTransition ();
+		} else if (!day && (time > MORNING && time < NIGHT)) {
+			day = true;
+			ProcessDayTransition ();
+		}
 	}
 
-    void SetHour(){
+	void SetHour ()
+	{
 		time = (12f + (transform.rotation.eulerAngles.z) / (360f / 24f)) % 24f;
 		hours = (int)time;
 		minutes = (int)((time % 1f) * 60f);
-		seconds =  (int)((time % 0.01f) * 60f);
+		seconds = (int)((time % 1f) * .6f);
+	}
+
+	public void AddNightCallBack (EventName eventName)
+	{
+		nightCallBack.Add (eventName);
+	}
+
+	public void AddDayCallBack (EventName eventName)
+	{
+		dayCallBack.Add (eventName);
+	}
+
+	void ProcessNightTransition ()
+	{
+		foreach (EventName ev in nightCallBack) {
+			EventManager.TriggerAction (ev, null);
+		}
+	}
+
+	void ProcessDayTransition ()
+	{
+		foreach (EventName ev in dayCallBack) {
+			EventManager.TriggerAction (ev, null);
+		}
 	}
 		
 }
--- Code/Etherion/Assets/Scripts/Sound/SoundSystem.cs
+++ Code/Etherion/Assets/Scripts/Sound/SoundSystem.cs
@@ -1,21 +1,141 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SoundSystem : MonoBehaviour {
+public class SoundSystem : MonoBehaviour
+{
+
+	public static string BACKGROUND_SOUND_TRIGGER = "backgroundSoundTrigger";
+	public static string DAY_SOUND_TRIGGER = "daySoundTrigger";
+	public static string NIGHT_SOUND_TRIGGER = "nightSoundTrigger";
+	public static string BATTLE_SOUND_TRIGGER = "battleSoundTrigger";
 
 
-	public AudioClip backround;
-	public AudioClip day;
-	public AudioClip night;
-	public AudioClip battle;
+	public AudioSource backround;
+	public AudioSource day;
+	public AudioSource night;
+	public AudioSource battle;
+
+	public SkyManager skyManager;
+
+	EventName backgroundTrigger;
+	EventName dayTrigger;
+	EventName nightTrigger;
+	EventName battleTrigger;
+
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+		OpenChannels ();
+		AddSkyCallBacks ();
+
+		backround.Play ();
+
+
+		if (skyManager.day)
+			day.Play ();
+		else
+			night.Play ();
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void AddSkyCallBacks ()
+	{
+		skyManager.AddDayCallBack (dayTrigger);
+		skyManager.AddNightCallBack (dayTrigger);
 	}
+
+	void OpenChannels ()
+	{
+		backgroundTrigger = new EventName (BACKGROUND_SOUND_TRIGGER);
+		EventManager.StartListening (backgroundTrigger, TriggerBackground);
+
+		dayTrigger = new EventName (DAY_SOUND_TRIGGER);
+		EventManager.StartListening (dayTrigger, TriggerDay);
+
+		//nightTrigger = new EventName (NIGHT_SOUND_TRIGGER);
+		//EventManager.StartListening (nightTrigger, TriggerNight);
+
+		battleTrigger = new EventName (BATTLE_SOUND_TRIGGER);
+		EventManager.StartListening (battleTrigger, TriggerBattle);
+	}
+
+	void CloseChannels ()
+	{
+		EventManager.StopListening (backgroundTrigger);
+		EventManager.StopListening (dayTrigger);
+		EventManager.StopListening (nightTrigger);
+		EventManager.StopListening (battleTrigger);
+	}
+
+	void TriggerBackground (object[] param)
+	{
+		if (backround == null) {
+			Debug.Log ("Missing AudioSource in SoundSystem");
+			return;
+		}
+		if (backround.isPlaying) {
+			backround.Stop ();
+		} else {
+			backround.Play ();
+		}
+	}
+
+	void TriggerDay (object[] param)
+	{
+		if (day == null || night == null) {
+			Debug.Log ("Missing AudioSource in SoundSystem");
+			return;
+		}
+		if (skyManager.day) { 
+			StopClipOnNextLoop (night);
+			PlayClipOnNextLoop (day);
+		} else {
+			StopClipOnNextLoop (day);
+			PlayClipOnNextLoop (night);
+		}
+	}
+
+	void TriggerBattle (object[] param)
+	{
+		if (battle) {
+			Debug.Log ("Missing AudioSource in SoundSystem");
+			return;
+		}
+		if (battle.isPlaying) {
+			battle.Stop ();
+		} else {
+			battle.Play ();
+		}
+	}
+
+	private void PlayClipOnNextLoop (AudioSource clip)
+	{
+		float delay = backround.clip.length - backround.time;
+		StartCoroutine (PlayClip (clip, delay));
+		Debug.Log (delay);
+	}
+
+	private void StopClipOnNextLoop (AudioSource clip)
+	{
+		float delay = backround.clip.length - backround.time;
+		StartCoroutine (StopClip (clip, delay));
+	}
+
+	private IEnumerator PlayClip (AudioSource source, float delay)
+	{
+		Debug.Log ("New Clip in Queue");
+		yield return new WaitForSeconds (delay);
+		source.Play ();
+		Debug.Log ("Before");
+		Debug.Log ("Day : " + day.isPlaying);
+		Debug.Log ("Night : " + night.isPlaying);
+	}
+
+	private IEnumerator StopClip (AudioSource source, float delay)
+	{
+		yield return new WaitForSeconds (delay);
+		source.Stop ();
+	}
+
 }
